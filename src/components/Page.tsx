436bc55c3dde7f98d3e6c8dbf59d6cbacb2c236e
blazethekid <blazepavars@gmail.com>
2024-12-06 12:45:00 -0500

    New logic for enabling visual editor

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,4 +1,4 @@
-import { StoryblokComponent } from "@storyblok/react/rsc";
+import { storyblokEditable, StoryblokComponent } from "@storyblok/react/rsc";
 
 export const Page = (params: any) => {
   const blocks = params.blok?.blocks || [];
@@ -7,10 +7,7 @@ export const Page = (params: any) => {
     <main>
       {blocks.length > 0 ? (
         blocks.map((blok: any) => (
-          <div key={blok._uid}>
-            {blok._editable && (
-              <div dangerouslySetInnerHTML={{ __html: blok._editable }} />
-            )}
+          <div key={blok._uid} {...storyblokEditable(blok)}>
             <StoryblokComponent blok={blok} />
           </div>
         ))
